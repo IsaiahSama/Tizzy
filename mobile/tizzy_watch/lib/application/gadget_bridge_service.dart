252c9e01129603b90520e9ae8cff33c134db9ba5
IsaiahSama <zeldaplayergl@gmail.com>
2025-05-25 19:22:30 -0700

    Fixed issue where data wasn't being encoded

--- mobile/tizzy_watch/lib/application/gadget_bridge_service.dart
+++ mobile/tizzy_watch/lib/application/gadget_bridge_service.dart
@@ -2,21 +2,24 @@
 
 import 'package:android_intent_plus/android_intent.dart';
 import 'package:tizzy_watch/domain/entities/tempo_message.dart';
+import 'dart:convert';
 
 class GadgetBridgeService {
 
   static Future<void> sendToBangle(String appFunctionName, Map<String, dynamic> data) async {
+    String args = jsonEncode(data);
     AndroidIntent intent = AndroidIntent( 
       action: 'com.banglejs.uart.tx',
       package:
           'com.espruino.gadgetbridge.banglejs',
-      arguments: {'line': "$appFunctionName(${data.toString()})"},
+      // arguments: {'line': "custom({'tempo': '$msg'})"},
+      arguments: {'line': "$appFunctionName($args)"},
     );
     await intent.sendBroadcast();
   }
 
   static Future<void> sendTempoMesssage(TempoMessage msg) async {
-    Map<String, dynamic> data = {'message': msg.content, 'color': msg.color};
+    Map<String, dynamic> data = {'message': msg.content};
     await sendToBangle('displayTempo', data);
   }
 }
