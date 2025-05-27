3f7c6d8e391f4379bf8e6480da95575aeac9e524
IsaiahSama <zeldaplayergl@gmail.com>
2025-05-27 06:32:47 -0700

    Bug fixes

--- watch/tizzy.app.js
+++ watch/tizzy.app.js
@@ -1,6 +1,10 @@
+const Layout = require("Layout");
+let drawTimeout;
+let currentMessage;
+
 const STATES = {
-    WATCH: "watch",
-    TEMPO: "tempo",
+	WATCH: "watch",
+	TEMPO: "tempo",
 };
 
 let currentState = STATES.WATCH;
@@ -10,11 +14,13 @@ let validStates = ["watch", "tempo"];
 
 // utils
 const changeState = (newState) => {
-    if (!validStates.includes(newState)) {
-        Terminal.println("Invalid state!");
-        return;
-    }
-    currentState = newState;
+	if (!validStates.includes(newState)) {
+		Terminal.println("Invalid state!");
+		return;
+	}
+	currentState = newState;
+	drawTimeout = null;
+	draw();
 };
 
 const handleDrag = (event) => {
@@ -39,17 +45,15 @@ const queueDraw = (delay) => {
 
 // Watch Draw
 
-const clockLayout = new Layout({
-	type: "v", c: [
-		{ type: "txt", font: "Vector:50", label: "12:00 XX", id: "time" },
-		{ type: "txt", font: "6x8:2", label: "XXX XX XXXX", id: "date" },
-		{ type: "txt", font: "6x8:3", label: "XXXXXXXXX", id: "dow" }
-	]
-}, { lazy: true });
-
-clockLayout.update();
-
 const drawWatch = () => {
+	let clockLayout = new Layout({
+		type: "v", c: [
+			{ type: "txt", font: "Vector:50", label: "12:00 XX", id: "time" },
+			{ type: "txt", font: "6x8:2", label: "XXX XX XXXX", id: "date" },
+			{ type: "txt", font: "6x8:3", label: "XXXXXXXXX", id: "dow" }
+		]
+	}, { lazy: true });
+	clockLayout.update();
 
 	let d = new Date();
 	let clock = require("locale").time(d, 1);
@@ -64,23 +68,30 @@ const drawWatch = () => {
 	clockLayout.dow.label = day;
 
 	// Preparing to draw
-	g.setBgColor(0, 0, 0);
-
 	clockLayout.render();
 };
 
 // Tempo Draw
-const displayTempo = (data) => { // Function called from Phone
-    let newMessage = data.message;
-    currentMessage = newMessage;
 
-    changeState(STATES.TEMPO);
+const displayTempo = (data) => { // Function called from Phone
+
+	let newMessage = data.message;
+	currentMessage = newMessage;
+
+	changeState(STATES.TEMPO);
 	draw(); // Trigger draw
 };
 
 const drawTempo = () => {
-    let message = currentMessage ?? "???";
-    E.showMessage(message, "Tizzy");
+	let message = currentMessage ?? "???";
+	const tempoLayout = new Layout(
+		{
+			type: "txt", font: "6x8", label: "X XXXX XXXX", id: "tempo"
+		}
+	);
+	tempoLayout.update();
+
+	tempoLayout.render();
 };
 
 // Global Draw
@@ -103,7 +114,7 @@ Bangle.on("drag", handleDrag);
 
 draw();
 
-Bangle.setUI("clock");
+// Bangle.setUI("clock");
 
 Bangle.loadWidgets();
 Bangle.drawWidgets();
