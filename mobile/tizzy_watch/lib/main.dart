21bb53228969977244f4fe254301e046bf815bcc
IsaiahSama <zeldaplayergl@gmail.com>
2025-05-30 07:05:03 -0700

    Can now test firebase messaging

--- mobile/tizzy_watch/lib/main.dart
+++ mobile/tizzy_watch/lib/main.dart
@@ -1,7 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final notificationSettings = await FirebaseMessaging.instance.requestPermission(provisional: true);
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+
+  print('FCM Token: $fcmToken');
+
+  // FirebaseMessaging messaging = FirebaseMessaging.instance;
+  // NotificationSettings settings = await messaging.requestPermission(
+  //   alert: true,
+  //   announcement: false,
+  //   badge: true,
+  //   carPlay: false,
+  //   criticalAlert: false,
+  //   provisional: false,
+  //   sound: true,
+  // );
   runApp(ProviderScope(child: MainApp()));
 }
