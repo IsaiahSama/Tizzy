b20e7a54f49df0d0b7f4204da2d17abc9783f280
IsaiahSama <zeldaplayergl@gmail.com>
2025-06-01 13:49:52 -0700

    Will no longer deal with SSL

--- server/app.py
+++ server/app.py
@@ -1,17 +1,17 @@
-import ssl
 from fastapi import FastAPI
 
 try:
     from server.routes import tempo
+    from server.utils.mongo_db import MongoClient
 except ImportError:
     from routes import tempo
+    from utils.mongo_db import MongoClient
 
 app = FastAPI()
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain("./.certs/cert.pem", "./.certs/key.pem")
-app.include_router(tempo.router)
+client = MongoClient()
 
+app.include_router(tempo.router)
 
 @app.get("/ping")
 async def ping():
