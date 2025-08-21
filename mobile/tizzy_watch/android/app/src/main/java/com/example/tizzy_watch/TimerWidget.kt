f4f795344490e53dffd5b58526d318fc00654429
IsaiahSama <zeldaplayergl@gmail.com>
2025-08-21 15:06:50 -0400

    Created a timer widget for the homescreen

---
+++ mobile/tizzy_watch/android/app/src/main/java/com/example/tizzy_watch/TimerWidget.kt
@@ -0,0 +1,38 @@
+package com.example.tizzy_watch
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+import es.antonborri.home_widget.HomeWidgetPlugin
+/**
+ * Implementation of App Widget functionality.
+ */
+class TimerWidget : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            // Get reference to SharedPreferences.
+            val widgetData = HomeWidgetPlugin.getData(context)
+            val views = RemoteViews(context.packageName, R.layout.timer_widget).apply {
+
+                val title = widgetData.getString("timer_title", null)
+                setTextViewText(R.id.timer_title, title ?: "No title set")
+
+                val duration = widgetData.getString("timer_duration", null)
+                setTextViewText(R.id.timer_duration, duration ?: "No duration set")
+            }
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
